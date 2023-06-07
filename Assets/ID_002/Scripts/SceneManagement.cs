f11b8bae98da4f8cdbc99703abd7bd860f2e3ca1
Youssef <s129059@ap.be>
2023-06-07 08:48:18 +0200

    Made CrossFade simpler, removed bugs

--- Assets/ID_002/Scripts/SceneManagement.cs
+++ Assets/ID_002/Scripts/SceneManagement.cs
@@ -4,19 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
-    public Animator transition;
-    public float transitionTime = 1f;
-    private bool startAnimationActivated = false;
+    [SerializeField] private Animator transition;
+    [SerializeField] private float transitionTime = 1f;
 
     public void LoadMainScene()
     {
-        startAnimationActivated = false;
         StartCoroutine(LoadSceneWithTransition(1));
     }
 
     public void LoadEndScene()
     {
-        startAnimationActivated = false;
         StartCoroutine(LoadSceneWithTransition(2));
     }
 
@@ -27,28 +24,10 @@ public class SceneManagement : MonoBehaviour
 
     private IEnumerator LoadSceneWithTransition(int sceneIndex)
     {
-        
         transition.SetTrigger("Start");
-        Debug.Log("Animation started!");
         yield return new WaitForSeconds(transitionTime);
-        if (!startAnimationActivated)
-        {
-            SceneManager.LoadScene(sceneIndex);
-            Debug.Log("Scene loaded");
-            ActivateStartAnimation();
-            yield break;
-        }
-        Debug.Log("Timer on!");
-        yield return new WaitForSeconds(transitionTime);
-        transition.SetTrigger("End");
-        Debug.Log("End triggered");
-        transition.SetTrigger("BackToNormal");
-        Debug.Log("backToNormal");
-    }
-
-    public void ActivateStartAnimation()
-    {
-        startAnimationActivated = true;
-        Debug.Log("StartAnimationActivated");
+        SceneManager.LoadScene(sceneIndex);
+        yield break;
+        
     }
 }
