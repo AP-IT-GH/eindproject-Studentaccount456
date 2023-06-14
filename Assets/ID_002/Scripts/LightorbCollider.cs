4ed633ca00f9dcd89e8dc9eb4a487a836acac699
Youssef <s129059@ap.be>
2023-06-14 19:34:40 +0200

    Create bugged version of script Lightorbcollider

--- Assets/ID_002/Scripts/LightorbCollider.cs
+++ Assets/ID_002/Scripts/LightorbCollider.cs
@@ -9,8 +9,6 @@ public class LightorbCollider : MonoBehaviour
     [SerializeField] private Animator bearAnimator;
     [SerializeField] private GameObject mooseGhostPrefab;
     [SerializeField] private GameObject bearGhostPrefab;
-    [SerializeField] private GameObject lightorbPrefab;
-    //[SerializeField] private TextMeshProUGUI scoreText;
 
     private const float MinY = 22f;
     private const float MaxY = 95f;
@@ -21,56 +19,97 @@ public class LightorbCollider : MonoBehaviour
     private const float DelayTime = 2f;
     // private const int ScoreIncrement = 10;
 
+    private List<Rigidbody> rigidbodies = new List<Rigidbody>();
+    private List<Animator> animators = new List<Animator>();
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.CompareTag("Lightorb"))
+        if (collision.collider.CompareTag("MooseGhost"))
         {
-            if (collision.collider.CompareTag("MooseGhost"))
-            {
-                mooseAnimator.SetTrigger("MooseDies");
-                InstantiateGhostObject(mooseGhostPrefab);
-            }
-            else if (collision.collider.CompareTag("BearGhost"))
-            {
-                bearAnimator.SetTrigger("BearDies");
-                InstantiateGhostObject(bearGhostPrefab);
-            }
+            mooseAnimator.SetTrigger("MooseDies");
+            GameObject ghost = InstantiateGhostObject(mooseGhostPrefab);
+            Rigidbody ghostRigidbody = ghost.GetComponent<Rigidbody>();
+            Animator ghostAnimator = ghost.GetComponent<Animator>();
 
-            //IncreaseScore();
-            StartCoroutine(DelayedResetPosition(collision.gameObject));
+            if (ghostRigidbody != null)
+            {
+                rigidbodies.Add(ghostRigidbody);
+                ghostRigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+            }
+            if (ghostAnimator != null)
+            {
+                animators.Add(ghostAnimator);
+                ghostAnimator.enabled = false;
+            }
         }
+        else if (collision.collider.CompareTag("BearGhost"))
+        {
+            bearAnimator.SetTrigger("BearDies");
+            GameObject ghost = InstantiateGhostObject(bearGhostPrefab);
+            Rigidbody ghostRigidbody = ghost.GetComponent<Rigidbody>();
+            Animator ghostAnimator = ghost.GetComponent<Animator>();
+
+            if (ghostRigidbody != null)
+            {
+                rigidbodies.Add(ghostRigidbody);
+                ghostRigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+            }
+            if (ghostAnimator != null)
+            {
+                animators.Add(ghostAnimator);
+                ghostAnimator.enabled = false;
+            }
+        }
+
+        //IncreaseScore();
+        StartCoroutine(DelayedResetPosition(collision.gameObject));
     }
 
-    private void InstantiateGhostObject(GameObject ghostPrefab)
+    private GameObject InstantiateGhostObject(GameObject ghostPrefab)
     {
         float randomX = Random.Range(MinX, MaxX);
         float randomY = Random.Range(MinY, MaxY);
 
         Vector3 position = new Vector3(randomX, randomY, Z);
-        Instantiate(ghostPrefab, position, Quaternion.identity);
+        GameObject ghost = Instantiate(ghostPrefab, position, Quaternion.identity);
+        return ghost;
     }
 
-    private void IncreaseScore()
-    {
-        // currentScore += ScoreIncrement;
-        //scoreText.text = currentScore.ToString();
-    }
-
-    private System.Collections.IEnumerator DelayedResetPosition(GameObject original)
+    private IEnumerator DelayedResetPosition(GameObject original)
     {
         yield return new WaitForSeconds(DelayTime);
-        Rigidbody rigidbody = original.GetComponent<Rigidbody>();
-        if (rigidbody != null)
+
+        if (original.CompareTag("MooseGhost") || original.CompareTag("BearGhost"))
         {
-            rigidbody.velocity = Vector3.zero;
-            rigidbody.angularVelocity = Vector3.zero;
-        }
-        else
-        {
-            Debug.LogError("Voeg RigidBody toe aan component!");
+            Rigidbody originalRigidbody = original.GetComponent<Rigidbody>();
+            if (originalRigidbody != null)
+            {
+                originalRigidbody.velocity = Vector3.zero;
+                originalRigidbody.angularVelocity = Vector3.zero;
+                originalRigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+                rigidbodies.Add(originalRigidbody);
+            }
+            else
+            {
+                Debug.LogError("Rigidbody component not found on the ghost object!");
+            }
+
+            original.transform.position = new Vector3(original.transform.position.x, DelayedY, original.transform.position.z);
+            original.transform.rotation = Quaternion.identity;
+
+            Debug.Log("Position reset for object: " + original.name);
         }
 
-        original.transform.position = new Vector3(original.transform.position.x, DelayedY, original.transform.position.z);
-        original.transform.rotation = Quaternion.identity;
+        foreach (Rigidbody rb in rigidbodies)
+        {
+            if (rb != null)
+                rb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+        }
+
+        foreach (Animator animator in animators)
+        {
+            if (animator != null)
+                animator.enabled = false;
+        }
     }
 }
